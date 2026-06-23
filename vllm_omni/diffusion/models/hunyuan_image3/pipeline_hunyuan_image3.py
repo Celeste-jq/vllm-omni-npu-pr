5324f45b639ec993c35795e833bd0c993988ea73
Celeste-jq <591998922@qq.com>
2026-06-23 14:54:40 +0800

    Revert "Use platform diffusion attention backend for Hunyuan Image3"
    This reverts commit c330dfc158625d549c53f91cabb4de1c73fa1271.

--- vllm_omni/diffusion/models/hunyuan_image3/pipeline_hunyuan_image3.py
+++ vllm_omni/diffusion/models/hunyuan_image3/pipeline_hunyuan_image3.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 import logging
+import os
 from collections.abc import Iterable
 from typing import Any
 
@@ -341,6 +342,11 @@ class HunyuanImage3Pipeline(
             )
         ]
         quant_config = od_config.quantization_config
+        os.environ["DIFFUSION_ATTENTION_BACKEND"] = "TORCH_SDPA"
+        logger.info(
+            "Setting attention backend to TORCH_SDPA. "
+            "HunyuanImage3Pipeline only supports TORCH_SDPA to handle mixed causal and full attention."
+        )
         self.model = HunyuanImage3Model(self.hf_config, quant_config=quant_config)
         self.transformer = self.model
         self.vae = DistributedAutoencoderKLHunyuan.from_config(self.hf_config.vae)
